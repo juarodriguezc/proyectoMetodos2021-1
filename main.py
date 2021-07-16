b1733f27f7d7ffebed0d9b0dc363f4b01ce4df2b
juarodriguezc <juarodriguezc@unal.edu.co>
2021-07-16 17:15:55 -0500

    First test opencv

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import cv2
-print("mecha mecha")
+
+img = cv2.imread('assets/img_template.jpeg' , 0)
+#-1, IMREAD_COLOR: Color image mode No transparent
+#0, IMREAD_FRAYSCALE: Grayscale mode
+#1, IMREAD_UNCHANGED: Color image Transparent background
+img = cv2.resize(img,(700,700))
+#img = cv2.rotate(img,cv2.cv2.ROTATE_90_CLOCKWISE)
+cv2.imwrite('assets/template_img_resized.jpeg',img)
+cv2.imshow('Image' , img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 print("Proyecto métodos 2021-1")
-print(cv2.__version__)
-print("subtitulo")
+
