7df7c58ee158aca4462ec66444d3122c2bef564c
juarodriguezc <juarodriguezc@unal.edu.co>
2021-07-16 23:53:55 -0500

    First color line detector

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+import numpy as np
 import cv2
-
-img = cv2.imread('assets/img_template.jpeg' , 0)
-#-1, IMREAD_COLOR: Color image mode No transparent
-#0, IMREAD_FRAYSCALE: Grayscale mode
-#1, IMREAD_UNCHANGED: Color image Transparent background
-img = cv2.resize(img,(700,700))
-#img = cv2.rotate(img,cv2.cv2.ROTATE_90_CLOCKWISE)
-cv2.imwrite('assets/template_img_resized.jpeg',img)
-cv2.imshow('Image' , img)
-cv2.waitKey(0)
+import random 
+import matplotlib.pyplot as plt
+#Load image from assets
+img = cv2.imread('assets/img_test_detection2.jpeg' , -1)
+    #-1, IMREAD_COLOR: Color image mode No transparent
+    #0, IMREAD_FRAYSCALE: Grayscale mode
+    #1, IMREAD_UNCHANGED: Color image Transparent background
+#Seet width and height
+width = 700
+height = 700
+#Resize the image
+img = cv2.resize(img,(width,height))
+#Create a copy of the image with HSV color
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+cv2.imwrite('assets/img_hsv_test.jpeg',hsv)
+#Set color range to extract
+lower_blue = np.array([110,50,50])
+upper_blue = np.array([130,255,255])
+#Create mask
+mask = cv2.inRange(hsv, lower_blue,upper_blue)
+#Create window with image
+cv2.imshow('Image' , mask)
+cv2. waitKey(0)
 cv2.destroyAllWindows()
-
-
-print("Proyecto métodos 2021-1")
-
+print(mask)
