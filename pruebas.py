8ea956edb0068ed3d866acadfeb6728600c6facd
juarodriguezc <juarodriguezc@unal.edu.co>
2021-07-17 22:10:56 -0500

    Cambios, lista deteccion terminada

--- pruebas.py
+++ pruebas.py
@@ -3,7 +3,7 @@ import interpolinomial as pol
 import VectorToPolinomio as VTP
 from sympy import *
 
-puntos = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10]]
+puntos = [[0, 1], [1, 3], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10]]
 polV = pol.polinomial(puntos)
 polP = VTP.vtp(polV)
 print(polV)
