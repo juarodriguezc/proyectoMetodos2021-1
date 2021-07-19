20f8a0a7209e110a7b2f22826899660abfc5933c
Nicolàs <nmejiabunal.edu.co>
2021-07-19 12:19:09 -0500

    correccionPolinomial

--- pruebas.py
+++ pruebas.py
@@ -1,14 +1,12 @@
 import matplotlib.pyplot as plt
 import interpolinomial as pol
-import VectorToPolinomio as VTP
 import interLagrange as lge
 from sympy import *
 
 puntos = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9], [9, 10]]
-#polV = pol.polinomial(puntos)
-#polP = VTP.vtp(polV)
+polP = pol.polinomial(puntos)
 lagP = lge.lagrange(puntos)
-#print(lagP)
-#print(polP)
+print(polP)
 print(lagP)
+#plot(polP)
 plot(lagP)
